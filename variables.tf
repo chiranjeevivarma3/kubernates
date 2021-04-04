57c185b69c153e869e722966eeba695e6b103218
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2021-04-04 11:11:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,8 @@
 variable "aws_region" {
   default = "us-east-1"
 }
-variable "vault_addr" {
-  default=""
-}
-variable "vault_token" {
-  default = ""
-}
+variable "vault_addr" {}
+variable "vault_token" {}
 variable "cluster-name" {
   default = "terraform-eks-demo"
   type    = string
